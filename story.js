066f4f335e6629b99275e02a51d4bbc72fb78d65
yash-khaitan <85894670+yash-khaitan@users.noreply.github.com>
2022-08-02 08:48:40 +0530

    Uploaded 2nd Last draft of story

--- story.js
+++ story.js
@@ -1,4 +1,3 @@
-
 function dis1_choice1_func() {
   document.getElementById("dis1_p1").style.display = "block";
   document.getElementById("dis1_p1").classList.add("opacity_animation");
@@ -7,35 +6,32 @@ function dis1_choice1_func() {
 
   btn_hide1();
   // console.log('button1');
-}  
+}
 
 function dis1_choice2_func() {
   document.getElementById("dis1_p2").style.display = "block";
   document.getElementById("dis1_p2").classList.add("opacity_animation");
 
   btn_hide1();
-}  
+}
+const btn_hide1 = () => {
+  document.getElementById("buttons1").style.display = "none";
+
+  document.getElementById("buttons2").style.display = "flex";
+  document.getElementById("buttons2").classList.add("opacity_animation");
+};
 
 function dis2_choice1_func() {
   document.getElementById("dis2_p1").style.display = "block";
   document.getElementById("dis2_p1").classList.add("opacity_animation");
   btn_hide2();
-}  
+}
 
 function dis2_choice2_func() {
   document.getElementById("dis2_p2").style.display = "block";
   document.getElementById("dis2_p2").classList.add("opacity_animation");
   btn_hide2();
 }
-
-
-const btn_hide1 = () => {
-  document.getElementById("buttons1").style.display = "none";
-
-  document.getElementById("buttons2").style.display = "block";
-  document.getElementById("buttons2").classList.add("opacity_animation");
-
-};
 
 const btn_hide2 = () => {
   document.getElementById("buttons2").style.display = "none";
